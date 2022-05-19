5f41032174ab6da3e2682c52b6b79ba01749992f
Himanshu Suryawanshi <62571754+himanshu-surya1c@users.noreply.github.com>
2022-05-19 17:30:32 +0530

    added images and styling of image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,15 @@
 import "./card.styles.css";
 
 const Card = ({ product }) => {
-  const { title, price } = product;
+  const { id, title, price, image } = product;
 
   return (
-    <div className="card-container">
+    <div className="card-container" key={id}>
+      <img
+        alt={`product ${title}`}
+        src={image}
+        style={{ height: "180px", width: "180px" }}
+      />
       <h2>{title}</h2>
       <p>{price}</p>
     </div>
